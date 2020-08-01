da3ebcfcdda5cbe5790ff8a2adf359dc206aa9ff
chincy11 <chincy-wm19@student.tarc.edu.my>
2020-08-01 22:56:27 +0800

    commit by Chin Chung Yi

---
+++ app/src/main/java/com/example/hellotoast21/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.hellotoast21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    public static final String EXTRA_NUMBER
+            = "com.example.hellotoast21.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+    public void sayHello(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        String  num = mShowCount.getText().toString();
+        intent.putExtra(EXTRA_NUMBER, num);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
